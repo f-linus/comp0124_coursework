546de3aafd9afbe318cdcd32730a8ca2005366b1
Linus Folkerts <dev@li-f.de>
2024-03-14 15:39:10 +0000

    Big behaviour update

--- comp0124_coursework/agent/ant.py
+++ comp0124_coursework/agent/ant.py
@@ -2,49 +2,181 @@ import numpy as np
 
 
 class Ant:
-    def __init__(self, name, env, position: tuple, rotation=0) -> None:
+    def __init__(
+        self,
+        name,
+        env,
+        position: tuple,
+        rotation=0,
+        pheremone_intensity_coefficient=0.001,
+    ) -> None:
         self.name = name
         self.env = env
         self.pos = position
-        self.rotation = rotation
-        self.carrying_food = False
-
-    def step(self):
-        current_pos_discrete = (
+        self.pos_discrete = (
             int(self.pos[0]),
             int(self.pos[1]),
         )
+        self.rotation = rotation
+        self.carrying_food = False
+        self.last_nest_visit = env.iteration
+        self.found_food_at = None
 
-        # update pheromone layer
-        self.env.grid.food_pheromone_layer[current_pos_discrete] = 1
+        self.no_of_navigation_probes = 10
+        self.probing_distance = 5
+        self.pheremone_intensity_coefficient = pheremone_intensity_coefficient
+        self.rotation_noise = 0.2
+        self.movement_speed = 1
+        self.food_detection_box_size = 5
 
-        # random change in rotation
-        self.rotation += np.random.uniform(-0.5, 0.5)
-        self.rotation = self.rotation % (2 * np.pi)
-
-        # move forward
-        desired_new_pos = (
-            self.pos[0] + np.cos(self.rotation),
-            self.pos[1] + np.sin(self.rotation),
+    def determine_ideal_direction(self):
+        probing_directions = np.linspace(
+            self.rotation - np.pi / 2,
+            self.rotation + np.pi / 2,
+            self.no_of_navigation_probes,
         )
 
-        # convert to discrete positions
-        desired_pos_discrete = (
-            int(desired_new_pos[0]),
-            int(desired_new_pos[1]),
+        probing_positions_discrete = [
+            (
+                int(self.pos[0] + self.probing_distance * np.cos(direction)),
+                int(self.pos[1] + self.probing_distance * np.sin(direction)),
+            )
+            for direction in probing_directions
+        ]
+
+        if self.carrying_food:
+            # probe food pheromone layer
+            concentrations = []
+            for pos in probing_positions_discrete:
+                if (
+                    0 <= pos[0] < self.env.grid.size[0]
+                    and 0 <= pos[1] < self.env.grid.size[1]
+                ):
+                    concentrations.append(self.env.grid.home_pheromone_layer[pos])
+                else:
+                    concentrations.append(0)
+
+            # choose direction with highest concentration
+            if np.max(concentrations) == 0:
+                new_rotation = self.rotation
+            else:
+                new_rotation = probing_directions[np.argmax(concentrations)]
+        else:
+            # probe home pheromone layer
+            concentrations = []
+            for pos in probing_positions_discrete:
+                if (
+                    0 <= pos[0] < self.env.grid.size[0]
+                    and 0 <= pos[1] < self.env.grid.size[1]
+                ):
+                    concentrations.append(self.env.grid.food_pheromone_layer[pos])
+                else:
+                    concentrations.append(0)
+
+            # choose direction with highest concentration
+            if np.max(concentrations) == 0:
+                new_rotation = self.rotation
+            else:
+                new_rotation = probing_directions[np.argmax(concentrations)]
+        return new_rotation
+
+    def move(self):
+        # depending on whether the ant is carrying food or not, move towards the nest or the food
+        # probe the relevant pheromone layer to decide the direction
+        new_rotation = self.determine_ideal_direction()
+
+        # add random noise to direction
+        new_rotation += np.random.uniform(-self.rotation_noise, self.rotation_noise)
+
+        # move to new position if possible, if not possible flip direction
+        new_position = (
+            self.pos[0] + self.movement_speed * np.cos(new_rotation),
+            self.pos[1] + self.movement_speed * np.sin(new_rotation),
+        )
+        new_position_discrete = (
+            int(new_position[0]),
+            int(new_position[1]),
         )
 
-        # check if new position is within bounds and not occupied
         if (
-            0 <= desired_pos_discrete[0] < self.env.grid.size[0]
-            and 0 <= desired_pos_discrete[1] < self.env.grid.size[1]
-        ) and self.env.grid.physical_layer[desired_pos_discrete] == 0:
-            # update position
-            self.pos = desired_new_pos
+            0 <= new_position_discrete[0] < self.env.grid.size[0]
+            and 0 <= new_position_discrete[1] < self.env.grid.size[1]
+        ) and self.env.grid.physical_layer[new_position_discrete] == 0:
+            # remove ant from old position
+            self.env.grid.physical_layer[self.pos_discrete] = 0
 
-            # update grid
-            self.env.grid.physical_layer[current_pos_discrete] = 0
-            self.env.grid.physical_layer[desired_pos_discrete] = 1
+            self.pos = new_position
+            self.pos_discrete = new_position_discrete
+            self.rotation = new_rotation
+
+            # add ant to new position
+            self.env.grid.physical_layer[self.pos_discrete] = 1
+        else:
+            self.rotation = self.rotation + np.pi
+        return
+
+    def deposit_pheromone(self):
+        if self.carrying_food:
+            pheremone_concentration = 1000 * np.exp(
+                self.pheremone_intensity_coefficient
+                * -1
+                * (self.env.iteration - self.found_food_at)
+            )
+            self.env.grid.food_pheromone_layer[self.pos_discrete] = np.max(
+                [
+                    self.env.grid.food_pheromone_layer[self.pos_discrete],
+                    pheremone_concentration,
+                ]
+            )
+        else:
+            pheremone_concentration = 1000 * np.exp(
+                self.pheremone_intensity_coefficient
+                * -1
+                * (self.env.iteration - self.last_nest_visit)
+            )
+            self.env.grid.home_pheromone_layer[self.pos_discrete] = np.max(
+                [
+                    self.env.grid.home_pheromone_layer[self.pos_discrete],
+                    pheremone_concentration,
+                ]
+            )
+
+    def detect_food(self):
+        # create box bounds that stay in bounds
+        box_bounds = [
+            (
+                int(self.pos[0] - self.food_detection_box_size),
+                int(self.pos[1] - self.food_detection_box_size),
+            ),
+            (
+                int(self.pos[0] + self.food_detection_box_size),
+                int(self.pos[1] + self.food_detection_box_size),
+            ),
+        ]
+        box_bounds = [
+            (max(0, box_bounds[0][0]), max(0, box_bounds[0][1])),
+            (
+                min(self.env.grid.size[0], box_bounds[1][0]),
+                min(self.env.grid.size[1], box_bounds[1][1]),
+            ),
+        ]
+
+        # check if food is in box
+        for i in range(box_bounds[0][0], box_bounds[1][0]):
+            for j in range(box_bounds[0][1], box_bounds[1][1]):
+                if self.env.grid.physical_layer[i, j] == 2:
+                    self.carrying_food = True
+                    self.found_food_at = self.env.iteration
+                    self.env.grid.physical_layer[i, j] = 0
+
+                    # flip rotation
+                    self.rotation = self.rotation + np.pi
+                    return
+
+    def step(self):
+        self.move()
+        self.detect_food()
+        self.deposit_pheromone()
 
     def __hash__(self):
         return hash(self.name)
--- comp0124_coursework/model/base_environment.py
+++ comp0124_coursework/model/base_environment.py
@@ -17,64 +17,61 @@ class Grid:
         self.size = size
 
         self.physical_layer = np.zeros(size, dtype=np.int8)
+        self.home_pheromone_layer = np.zeros(size, dtype=np.float32)
         self.food_pheromone_layer = np.zeros(size, dtype=np.float32)
 
 
 class BaseEnvironment:
-    def __init__(self, size: tuple, nest_location: tuple, logging_interval=40) -> None:
+    def __init__(self):
+        pass
+
+    def run(
+        self,
+        size: tuple,
+        nest_location: tuple,
+        logging_interval=40,
+        no_ants=50,
+        no_iterations=10000,
+        render_to_screen=False,
+        render_interval=10,
+        render_step_wait_time=1,
+        save_rendering_interval=1000,
+        extra_iterations_to_save_rendering=[10, 100, 500],
+    ):
         self.grid = Grid(size)
         self.nest_location = nest_location
         self.ants = set()
         self.logging_interval = logging_interval
         self.avg_step_time = 0
         self.avg_render_time = 0
+        self.iteration = 1
+        self.pheremonone_decay_rate = 1
 
-    def run(
-        self,
-        no_ants=50,
-        no_iterations=1000,
-        render=False,
-        render_interval=10,
-        render_step_wait_time=1,
-        iterations_to_save_rendering=[10, 100, 500, 1000],
-    ):
+        self.render_to_screen = render_to_screen
+        self.render_interval = render_interval
+        self.render_step_wait_time = render_step_wait_time
+        self.save_rendering_interval = save_rendering_interval
+        self.extra_iterations_to_save_rendering = extra_iterations_to_save_rendering
+
         # spawn ants
-        for i in range(no_ants):
+        for self.iteration in range(no_ants):
             rnd_rotation = np.random.uniform(0, 2 * np.pi)
 
-            ant = Ant(f"ant_{i}", self, self.nest_location, rnd_rotation)
+            ant = Ant(f"ant_{self.iteration}", self, self.nest_location, rnd_rotation)
             self.ants.add(ant)
 
-        if render:
+        # spawn some food
+        self.grid.physical_layer[200:210, 200:210] = Grid.FOOD
+
+        if render_to_screen:
             cv2.namedWindow("env_vis", cv2.WINDOW_NORMAL)
 
-        for i in range(1, no_iterations + 1):
-            if render and i % render_interval == 0:
-                start_time = time.time()
-                image = self.render()
-                cv2.imshow("env_vis", image)
-
-                cv2.waitKey(render_step_wait_time)
-                self.avg_render_time += (
-                    (time.time() - start_time)
-                    * 1000
-                    / render_interval
-                    / self.logging_interval
-                )
-
-            if i in iterations_to_save_rendering:
-                if not render or i % render_interval != 0:
-                    image = self.render()
-                cv2.imwrite(f"render_{i}.png", image * 255)
-
+        while self.iteration <= no_iterations:
+            self.render_handling()
             self.step()
+            self.log_handling()
 
-            if i % self.logging_interval == 0 and i != 0:
-                logger.info(
-                    f"Step: {i} - Total step time: {self.avg_step_time + self.avg_render_time:.2f}ms (step time: {self.avg_step_time:.2f}ms - render time: {self.avg_render_time:.2f}ms)"
-                )
-                self.avg_step_time = 0
-                self.avg_render_time = 0
+            self.iteration += 1
 
         cv2.destroyAllWindows()
 
@@ -89,29 +86,76 @@ class BaseEnvironment:
 
         self.avg_step_time += (time.time() - start_time) * 1000 / self.logging_interval
 
-    def render(self):
-        # food phereomone layer
-        image = np.repeat(
-            self.grid.food_pheromone_layer[:, :, np.newaxis], 3, axis=2
-        ) * [1, 0.3, 0.3]
-
-        # ants
-        image[self.grid.physical_layer == Grid.ANT] = [1.0, 1.0, 1.0]
-
-        return image
-
     def pheremonone_decay(self):
         self.grid.food_pheromone_layer = np.max(
             np.stack(
-                [self.grid.food_pheromone_layer - 0.004, np.zeros(self.grid.size)],
+                [
+                    self.grid.food_pheromone_layer - self.pheremonone_decay_rate,
+                    np.zeros(self.grid.size),
+                ],
                 axis=2,
             ),
             axis=2,
         )
 
+    def log_handling(self):
+        if self.iteration % self.logging_interval == 0 and self.iteration != 0:
+            logger.info(
+                f"Step: {self.iteration} - Total step time: {self.avg_step_time + self.avg_render_time:.2f}ms (step time: {self.avg_step_time:.2f}ms - render time: {self.avg_render_time:.2f}ms)"
+            )
+            self.avg_step_time = 0
+            self.avg_render_time = 0
+
+    def render_handling(
+        self,
+    ):
+        if self.render_to_screen and self.iteration % self.render_interval == 0:
+            start_time = time.time()
+            image = self.render()
+            cv2.imshow("env_vis", image)
+
+            cv2.waitKey(self.render_step_wait_time)
+            self.avg_render_time += (
+                (time.time() - start_time)
+                * 1000
+                / self.render_interval
+                / self.logging_interval
+            )
+
+        if self.iteration in self.extra_iterations_to_save_rendering or (
+            self.iteration % self.save_rendering_interval == 0
+        ):
+            if not self.render_to_screen or self.iteration % self.render_interval != 0:
+                image = self.render()
+            cv2.imwrite(f"render_{self.iteration}.png", image * 255)
+
+    def render(self):
+        # food phereomone layer
+        home_layer = (
+            np.repeat(self.grid.home_pheromone_layer[:, :, np.newaxis], 3, axis=2)
+            * [1, 0.3, 0.3]
+            / 1000
+        )
+
+        food_layer = (
+            np.repeat(self.grid.food_pheromone_layer[:, :, np.newaxis], 3, axis=2)
+            * [0.3, 1, 0.3]
+            / 1000
+        )
+
+        image = np.clip(home_layer + food_layer, 0, 1)
+
+        # ants
+        image[self.grid.physical_layer == Grid.ANT] = [1.0, 1.0, 1.0]
+
+        # food
+        image[self.grid.physical_layer == Grid.FOOD] = [0.0, 0.0, 1.0]
+
+        return image
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    env = BaseEnvironment((1000, 1000), (500, 500))
-    env.run(render=False)
+    env = BaseEnvironment()
+    env.run((600, 600), (300, 300), render_to_screen=False)
